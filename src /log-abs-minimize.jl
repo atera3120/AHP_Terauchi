9786c11dbc7cdd0df75dacbc6fea9c7c121114f3
atera3120 <147809664+atera3120@users.noreply.github.com>
2023-10-17 07:03:58 +0000

    デバッグの必要あり

--- src /log-abs-minimize.jl
+++ src /log-abs-minimize.jl
@@ -5,14 +5,7 @@ import HiGHS
 include("./crisp-pcm.jl")
 include("./nearly-equal.jl")
 
-LPResult_Individual = @NamedTuple{
-    # 区間重みベクトル
-    wᴸ::Vector{T}, wᵁ::Vector{T},
-    W::Vector{Interval{T}}, # ([wᵢᴸ, wᵢᵁ])
-    optimalValue::T
-    } where {T <: Real}
-
-function AbsErrMin(A::Matrix{T})::T where {T <: Real}
+function LogAbsErrMin(A::Matrix{T})::T where {T <: Real}
     ε = 1e-8 # << 1
 
     if !isCrispPCM(A)
@@ -25,18 +18,16 @@ function AbsErrMin(A::Matrix{T})::T where {T <: Real}
 
     try
         @variable(model, u[i=1:n]);
-        @variable(model, U[i=1:n; j=i+1:n]);
-        @variable(model, ∑∑Uᵢⱼ);
+        @variable(model, U[i=1:n, j=i+1:n]);
         
         # 上三角成分に対応する i, j
         ∑∑Uᵢⱼ = 0
         for i = 1:n-1
             for j = i+1:n
-                wᵢᴸ = wᴸ[i]; wᵢᵁ = wᵁ[i]
                 uᵢ = u[i]; uⱼ = u[j]; Uᵢⱼ = U[i,j]
                 aᵢⱼ = A[i,j]
-                @constraint(model, log(e, aᵢⱼ) - uᵢ + uⱼ <= Uᵢⱼ)
-                @constraint(model, -log(e, aᵢⱼ) + uᵢ - uⱼ <= Uᵢⱼ)
+                @constraint(model, log(ℯ, aᵢⱼ) - uᵢ + uⱼ <= Uᵢⱼ)
+                @constraint(model, -log(ℯ, aᵢⱼ) + uᵢ - uⱼ <= Uᵢⱼ)
                 ∑∑Uᵢⱼ += Uᵢⱼ
             end
         end
@@ -45,8 +36,12 @@ function AbsErrMin(A::Matrix{T})::T where {T <: Real}
         @objective(model, Min, ∑∑Uᵢⱼ)
 
         optimize!(model)
-        temp = exp(value.(u))
-        OptimalValue = sum(temp)
+        temp = exp.(value.(u))
+        optimalValue = temp/sum(temp)
+
+        return (
+            optimalValue=optimalValue
+        )
     
     finally
         # エラー終了時にも変数などを消去する
